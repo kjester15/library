98d334bb51381673de8c625fe341bc0acd954e16
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-06-28 17:19:28 -0700

    Complete working displayLibrary function to add books in library to html

--- application.js
+++ application.js
@@ -1,13 +1,10 @@
-const myLibrary = ["hi", "bye"]
+const myLibrary = []
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.info = function() {
-    // return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  // };
 }
 
 function addBookToLibrary(book) {
@@ -15,7 +12,27 @@ function addBookToLibrary(book) {
 }
 
 function displayLibrary(array) {
-  array.forEach((element) => console.log(element));
+  array.forEach((element) => {
+    const newDiv = document.createElement("div");
+    document.getElementById("book-shelf").appendChild(newDiv);
+    const newTitle = document.createElement("h3");
+    newTitle.innerHTML = element.title;
+    const newAuthor = document.createElement("h4");
+    newAuthor.innerHTML = element.author;
+    const newPages = document.createElement("h5");
+    newPages.innerHTML = element.pages;
+    const newRead = document.createElement("div");
+    newRead.innerHTML = element.read;
+    newDiv.append(newTitle, newAuthor, newPages, newRead);
+    newDiv.classList.add("book-card");
+  });
 }
 
-displayLibrary(myLibrary)
+const book1 = new Book("title 1", "author 1", 395, true);
+addBookToLibrary(book1);
+const book2 = new Book("title 2", "author 2", 395, true);
+addBookToLibrary(book2);
+const book3 = new Book("title 3", "author 3", 395, true);
+addBookToLibrary(book3);
+displayLibrary(myLibrary);
+document.getElementById('add').addEventListener("click", addBookToLibrary);
